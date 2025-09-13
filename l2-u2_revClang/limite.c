aed6b8592ef913500cef8ba2396900dcd4c14074
Jefferson <jeffersontcontact@gmail.com>
2025-09-13 19:29:32 +0000

    add limitec working fine

--- l2-u2_revClang/limite.c
+++ l2-u2_revClang/limite.c
@@ -6,36 +6,46 @@
 #include <ctype.h>
 #include <stdbool.h>
 
+bool isVogal(char p){
+    bool check=false;
+  
+    if(p=='A' || p=='E' || p=='I' ||
+    p=='O' || p=='U'){
+        check=true;
+    }
 
-/*DEVE SER RECURSIVA!!!*/
-int countC(char* pal[], char l){
+    return check;
+}
+
+char* upperChar(char pal[]){
+    int tam=strlen(pal);
+    for(int k=0;k<tam;k++) pal[k]=toupper(pal[k]);
+    return pal;
+}
+
+int countC(char pal[], char l){
     int tam=0;
     int count=0;
 
     //converte para maisculo
-    pal=toupper(pal);
+    upperChar(pal);
     l=toupper(l);
-    tam=len(pal);
+    tam=strlen(pal);
 
-    if(*pal=='\0') return 0;
+    if(*pal=='\0') return 0; 
 
-    char vpal[50];
+    char at=pal[count];
 
-    //seleciona apenas consoantes
-    for(int i=0; i<tam; i++){
-        if(!isVogal(pal[i])){
-            vpal[i]=pal[i];
-        }
-    }
+    // 'pal+1' abaixo permite avançar com base 
+    // no índice de cada posição das letras da string
 
-    char at=vpal;
-
-    if(at>l) count=1;
+    if(!isVogal(at) && at>l && at>='A' && at<='Z') return 1+countC(pal+1, l);
 
     return count+countC(pal+1,l);
 }
 
 /*
+f alternativa de conversão para caixa alta 
 void convertCase(char* p[]){
     char convert_pal[50];
     int i=0;
@@ -46,25 +56,20 @@ void convertCase(char* p[]){
 }
 */
 
-bool isVogal(char p){
-    bool check=false;
-  
-    if(p=='a' || p=='e' || p='i' ||
-    p=='o' || p=='u'){
-        check=true;
-    }
-
-    return check;
-}
-
 int main(){
     char c;
     char palavra[100];
 
-    scanf("%s", &c);
+    scanf("%c", &c);//toma a letra limite 
+    getchar(); //consome nova linha
+
     fgets(palavra, 100, stdin);
+    palavra[strcspn(palavra, "\n")]='\0'; //consome nova linha para palavra
     
-    countC(palavra, c);
+    int valorCnst=countC(palavra, c);
+    char novo_c=toupper(c);
+    printf("\nSão %d consoantes depois do caractere '%c'", valorCnst, novo_c);
+    printf("\n\n");
 
     return 0;
 }
