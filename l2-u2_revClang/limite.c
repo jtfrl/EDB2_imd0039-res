983169ed300465b7deb7e4a0d6a248923461c252
Jefferson <jeffersontcontact@gmail.com>
2025-09-13 04:45:23 +0000

    add program still to be adjusted

--- l2-u2_revClang/limite.c
+++ l2-u2_revClang/limite.c
@@ -3,29 +3,55 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <ctype.h>
 #include <stdbool.h>
 
 
 /*DEVE SER RECURSIVA!!!*/
 int countC(char* pal[], char l){
-    int tam, count, vog, cnt;
-    //função que converte para lower
+    int tam=0;
+    int count=0;
+
+    //converte para maisculo
+    pal=toupper(pal);
+    l=toupper(l);
     tam=len(pal);
 
+    if(*pal=='\0') return 0;
 
+    char vpal[50];
 
+    //seleciona apenas consoantes
+    for(int i=0; i<tam; i++){
+        if(!isVogal(pal[i])){
+            vpal[i]=pal[i];
+        }
+    }
 
-    return count;
+    char at=vpal;
+
+    if(at>l) count=1;
+
+    return count+countC(pal+1,l);
 }
 
+/*
+void convertCase(char* p[]){
+    char convert_pal[50];
+    int i=0;
+    while(p[i]!='\0'){ //até o último caractere
+        p[i]=p[i]-32;
+        i++;
+    }
+}
+*/
 
-bool isVogal(char* p[]){
+bool isVogal(char p){
     bool check=false;
-    for(int i=0; i<len(p); i++){
-        if(p[i]=='a' || p[i]=='e' || p[i]=i ||
-        p[i]=='o' || p[i]=='u'){
-            check=true;
-        }
+  
+    if(p=='a' || p=='e' || p='i' ||
+    p=='o' || p=='u'){
+        check=true;
     }
 
     return check;
