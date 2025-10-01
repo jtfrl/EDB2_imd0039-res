99bfc04be67b4d3deb06b168f42f01ae73070ee0
Jefferson <jeffersontcontact@gmail.com>
2025-10-01 00:48:34 +0000

    add item (a); still to be impl

--- trees/arvSearchBin/asbin.c
+++ trees/arvSearchBin/asbin.c
@@ -3,20 +3,47 @@
 int main(void) {
     int valores[] = {8, 3, 10, 1, 6, 14, 4, 7, 13};
     int n = (int)(sizeof(valores) / sizeof(valores[0]));
+    
+    int val_a[]={50, 30, 70, 20, 40, 60, 80, 10, 25, 35, 45, 55, 65, 75, 85};
+    int n_a=(int)(sizeof(val_a)/sizeof(val_a[0]));
 
     No* raiz = NULL;
     for (int i = 0; i < n; ++i) raiz = inserir(raiz, valores[i]);
+
+    No* raiz_a = NULL;
+    for (int j = 0; j < n_a; ++j) raiz_a = inserir(raiz_a, val_a[j]); //caso fosse o limite com n
+    //... o unico valor divisivel por 10 incluído seria 25
 
     printf("Em-ordem (ordenado): ");
     emOrdem(raiz);
     printf("\n");
 
+    //__________
+    //item a)
+    printf("Em-ordem (ordenado): ");
+    emOrdem(raiz_a); //não está incluindo 55 (???)
+    printf("\n");
+    //__________
+
     int x = 7;
     printf("Buscando %d... %s\n", x, buscar(raiz, x) ? "encontrado" : "nao encontrado");
+
+
+    //__________
+    //item a)
+    int x2 = 55;
+    printf("Buscando %d... %s\n", x2, buscar(raiz_a, x2) ? "encontrado" : "nao encontrado");
+    //__________
 
     raiz = remover(raiz, 1);
     raiz = remover(raiz, 14);
     raiz = remover(raiz, 3);
+
+     //__________
+    //item a)
+    //REMOÇÃO
+     //__________
+   
 
     printf("Em-ordem após remoções: ");
     emOrdem(raiz);
